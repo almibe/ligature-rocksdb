7db0afba87be24ce4117f16ba602cb19aff7e0b3
Alex Berry <berry3@indiana.edu>
2016-02-13 02:27:32 -0500

    Adding basic project structure

---
+++ src/main/kotlin/org/libraryweasel/stinkpot/burrow/Activator.kt
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.libraryweasel.stinkpot.burrow
+
+import org.apache.felix.dm.DependencyActivatorBase
+import org.apache.felix.dm.DependencyManager
+import org.libraryweasel.servo.LibraryWeaselComponentRegistrar
+import org.osgi.framework.BundleContext
+
+class Activator() : DependencyActivatorBase {
+    @Override
+    fun init(context: BundleContext, manager: DependencyManager) {
+        registrar = LibraryWeaselComponentRegistrar(manager)
+    }
+
+    @Override
+    fun destroy(context: BundleContext, manager: DependencyManager) {
+
+    }
+}
