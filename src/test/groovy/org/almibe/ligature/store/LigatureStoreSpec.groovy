633d4442ae6e3ec8ac81b6bfad4fd4323f41ae4d
Alex Berry <berry3@indiana.edu>
2017-08-20 20:03:27 -0400

    updating tests

--- src/test/groovy/org/almibe/ligature/store/LigatureStoreSpec.groovy
+++ src/test/groovy/org/almibe/ligature/store/LigatureStoreSpec.groovy
@@ -7,6 +7,7 @@ package org.almibe.ligature.store
 import com.orientechnologies.orient.core.db.ODatabasePool
 import com.orientechnologies.orient.core.db.OrientDB
 import com.orientechnologies.orient.core.db.OrientDBConfig
+import kotlin.Pair
 import org.almibe.ligature.IRI
 import org.almibe.ligature.LangLiteral
 import org.almibe.ligature.TypedLiteral
@@ -24,7 +25,6 @@ class LigatureStoreSpec extends Specification {
     def store = new OrientDBLigatureStore(pool)
 
     def setupSpec() {
-
     }
 
     def cleanupSpec() {
@@ -35,31 +35,45 @@ class LigatureStoreSpec extends Specification {
     def "check saving a simple triple made of three IRIs"() {
         when:
         store.addStatement(new IRI("http://example.org/#spiderman"),
-                new IRI("http://www.perceive.net/schemas/relationship/enemyOf"), new IRI("http://example.org/#green-goblin"))
+                new IRI("http://www.perceive.net/schemas/relationship/enemyOf"),
+                new IRI("http://example.org/#green-goblin"))
         then:
-        testGraph.getVerticesOfClass("IRI").size() == 2
-        testGraph.getEdgesOfClass("PredicateIRI").size() == 1
+        store.IRIs.size() == 3
+        store.statementsFor(new IRI("http://example.org/#spiderman")).first() ==
+                new Pair(new IRI("http://www.perceive.net/schemas/relationship/enemyOf"),
+                new IRI("http://example.org/#green-goblin"))
     }
 
     def "support sharing vertices if IRI is the same"() {
         when:
         store.addStatement(new IRI("http://example.org/#spiderman"),
-                new IRI("http://www.perceive.net/schemas/relationship/enemyOf"), new IRI("http://example.org/#black-cat"))
+                new IRI("http://www.perceive.net/schemas/relationship/enemyOf"),
+                new IRI("http://example.org/#black-cat"))
         then:
-        testGraph.getVerticesOfClass("IRI").size() == 3
-        testGraph.getEdgesOfClass("PredicateIRI").size() == 2
+        store.IRIs.size() == 4
+        store.objects.size() == 2
+        store.statementsFor(new IRI("http://example.org/#spiderman")).size() == 2
     }
 
     def "support literals in object"() {
         when:
-        store.addStatement(new IRI("http://example.org/show/218"), new IRI("http://www.w3.org/2000/01/rdf-schema#label"), new TypedLiteral("That Seventies Show", new IRI("http://www.w3.org/2001/XMLSchema#string")))
-        store.addStatement(new IRI("http://example.org/show/218"), new IRI("http://example.org/show/localName"), new LangLiteral("That Seventies Show", "en"))
+        store.addStatement(new IRI("http://example.org/show/218"),
+                new IRI("http://www.w3.org/2000/01/rdf-schema#label"),
+                new TypedLiteral("That Seventies Show", new IRI("http://www.w3.org/2001/XMLSchema#string")))
+        store.addStatement(new IRI("http://example.org/show/218"),
+                new IRI("http://example.org/show/localName"),
+                new LangLiteral("That Seventies Show", "en"))
         then:
-        testGraph.getVerticesOfClass("IRI").size() == 4
-        testGraph.getVerticesOfClass("Literal").size() == 3
-        testGraph.getVerticesOfClass("LangLiteral").size() == 1
-        testGraph.getVerticesOfClass("PlainLiteral").size() == 1
-        testGraph.getVerticesOfClass("TypedLiteral").size() == 1
+        store.IRIs.size() == 8
+        store.literals == [
+                new TypedLiteral("That Seventies Show", new IRI("http://www.w3.org/2001/XMLSchema#string")),
+                new LangLiteral("That Seventies Show", "en")].toSet()
+        store.statementsFor(new IRI("http://example.org/show/218")) == [
+                new Pair(new IRI("http://example.org/show/localName"),
+                    new LangLiteral("That Seventies Show", "en")),
+                new Pair(new IRI("http://www.w3.org/2000/01/rdf-schema#label"),
+                    new TypedLiteral("That Seventies Show", new IRI("http://www.w3.org/2001/XMLSchema#string")))
+        ].toSet()
     }
 
 //    def "support blank nodes"() {
