bb860f7c443778bd3df4a993ab35e10c9dfe1731
Alex Michael Berry <berry3@indiana.edu>
2019-07-18 21:03:10 -0400

    filling XodusEncodingSpec, #10

--- src/test/kotlin/org/almibe/ligature/store/XodusEncodingSpec.kt
+++ src/test/kotlin/org/almibe/ligature/store/XodusEncodingSpec.kt
@@ -4,24 +4,67 @@
 
 package org.almibe.ligature.store
 
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldThrow
 import io.kotlintest.specs.StringSpec
+import jetbrains.exodus.bindings.StringBinding
+import org.almibe.ligature.*
+import java.lang.RuntimeException
 
 class XodusEncodingSpec: StringSpec() {
     init {
         "test graph encoding/decoding" {
-            TODO()
+            val defaultGraph = DefaultGraph
+            val namedGraph = NamedGraph(IRI("http://test"))
+
+            defaultGraph shouldBe decodeGraph(StringBinding.entryToString(encodeGraph(defaultGraph)))
+            namedGraph shouldBe decodeGraph(StringBinding.entryToString(encodeGraph(namedGraph)))
         }
 
         "test subject encoding/decoding" {
-            TODO()
+            val iri = IRI("http://test")
+            val blankNode = BlankNode("blank")
+            val exception = object: Subject {}
+
+            iri shouldBe decodeSubject(StringBinding.entryToString(encodeSubject(iri)))
+            blankNode shouldBe decodeSubject(StringBinding.entryToString(encodeSubject(blankNode)))
+            shouldThrow<RuntimeException> {
+                encodeSubject(exception)
+            }
+            shouldThrow<RuntimeException> {
+                decodeSubject("throw")
+            }
         }
 
         "test predicate encoding/decoding" {
-            TODO()
+            val iri = IRI("http://test")
+            val exception = object: Predicate {}
+
+            iri shouldBe decodePredicate(StringBinding.entryToString(encodePredicate(iri)))
+            shouldThrow<RuntimeException> {
+                encodePredicate(exception)
+            }
+            shouldThrow<RuntimeException> {
+                decodePredicate("throw")
+            }
         }
 
         "test literal encoding/decoding" {
-            TODO()
+            val langLiteral = LangLiteral("test", "en")
+            val langLiteral2 = LangLiteral("test test@", "en-0a9")
+            val exception = LangLiteral("test test", "56")
+
+            val typedLiteral = TypedLiteral("5", IRI("http://test"))
+            val typedLiteral2 = TypedLiteral("5 ex^^ re^^", IRI("http://test"))
+
+            langLiteral shouldBe decodeLiteral(StringBinding.entryToString(encodeLiteral(langLiteral)))
+            langLiteral2 shouldBe decodeLiteral(StringBinding.entryToString(encodeLiteral(langLiteral2)))
+            shouldThrow<RuntimeException> {
+                decodeLiteral(StringBinding.entryToString(encodeLiteral(exception)))
+            }
+
+            typedLiteral shouldBe decodeLiteral(StringBinding.entryToString(encodeLiteral(typedLiteral)))
+            typedLiteral2 shouldBe decodeLiteral(StringBinding.entryToString(encodeLiteral(typedLiteral2)))
         }
     }
 }
