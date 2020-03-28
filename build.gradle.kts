94566adb8e757e7eff4f41701e8af77dd4ea532f
Alex Michael Berry <berry3@indiana.edu>
2020-03-28 01:02:31 -0400

    removing old code

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.xodus:xodus-environment:1.3.91")
+    implementation("org.jetbrains.xodus:xodus-environment:1.3.124")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
-    implementation("io.vavr:vavr:1.0.0-alpha-3")
     implementation("org.libraryweasel.ligature:ligature:0.1.0-SNAPSHOT")
     testImplementation("org.libraryweasel.ligature:ligature-test-suite:0.1.0-SNAPSHOT")
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
--- src/test/kotlin/org/libraryweasel/ligature/xodus/XodusSpec.kt
+++ src/test/kotlin/org/libraryweasel/ligature/xodus/XodusSpec.kt
@@ -1,2 +1,11 @@
-package org.almibe.ligature.store
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+package org.libraryweasel.ligature.xodus
+
+import io.kotlintest.specs.StringSpec
+import org.libraryweasel.ligature.xodus.XodusLigatureStore
+import org.libraryweasel.ligature.test.createSpec
+
+class InMemorySpec: StringSpec({createSpec { XodusLigatureStore() }()  })
